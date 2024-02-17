782a2c540b8d03614791839d78ae408f51deab6f
Eugene Yurtsev <eyurtsev@gmail.com>
2024-02-16 22:44:23 -0500

    x

--- backend/db/models.py
+++ backend/db/models.py
@@ -2,7 +2,7 @@ import uuid
 from contextlib import contextmanager
 from typing import Generator
 
-from sqlalchemy import Column, DateTime, String, create_engine
+from sqlalchemy import Column, DateTime, String, Text, create_engine
 from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session, sessionmaker
@@ -33,11 +33,38 @@ def get_session() -> Generator[Session, None, None]:
 class Extractor(Base):
     __tablename__ = "extractors"
 
-    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    modified_at = Column(DateTime(timezone=True), onupdate=func.now())
-    schema = Column(JSONB)
-    description = Column(String(100))
+    uuid = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        comment="Unique identifier for this extractor.",
+    )
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        comment="Time when this extracted was originally created.",
+    )
+    modified_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        onupdate=func.now(),
+        comment="Last time this was modified.",
+    )
+    schema = Column(
+        JSONB,
+        nullable=False,
+        comment="JSON Schema that describes what content will be extracted from the document",
+    )
+    description = Column(
+        String(100),
+        nullable=False,
+        server_default="",
+        comment="Surfaced via UI to the users.",
+    )
+    instruction = Column(
+        Text, nullable=False, comment="The prompt to the language model."
+    )  # TODO: This will need to evolve
 
     def __repr__(self):
         return f"<Extractor(id={self.id}, description={self.description})>"
