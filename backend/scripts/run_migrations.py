bff5c7852d0b1c81dac05895c18dfd480186fd5f
Eugene Yurtsev <eyurtsev@gmail.com>
2024-02-20 13:22:37 -0500

    Update run migrations script (#3)
    Place holder script but will make it easier to drop and re-recreate schema
    while experimenting

--- backend/scripts/run_migrations.py
+++ backend/scripts/run_migrations.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 """Run migrations."""
+import click
+
 from db.models import ENGINE, Base
 
 
-def main() -> None:
+@click.group()
+def cli():
+    """Database migration commands."""
+    pass
+
+
+@cli.command()
+def create():
     """Create all tables."""
     Base.metadata.create_all(ENGINE)
+    click.echo("All tables created successfully.")
+
+
+@cli.command()
+@click.confirmation_option(prompt="Are you sure you want to drop all tables?")
+def drop():
+    """Drop all tables."""
+    Base.metadata.drop_all(ENGINE)
+    click.echo("All tables dropped successfully.")
 
 
 if __name__ == "__main__":
-    print("Running migrations...")
-    print("For now this is a simple script that creates all tables.")
-    main()
+    cli()
