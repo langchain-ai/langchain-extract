b5ab0280aead14db72c779954bbcd3dbff8aa002
ccurme <chester.curme@gmail.com>
2024-02-22 12:05:53 -0500

    accommodate long docs (#18)

--- backend/server/main.py
+++ backend/server/main.py
@@ -1,25 +1,22 @@
 """Entry point into the server."""
-from typing import Any, Dict, List, Optional
+from typing import Literal
 from uuid import UUID
 
 from fastapi import Depends, FastAPI, HTTPException
-from jsonschema import Draft202012Validator, exceptions
-from langchain.chains.openai_functions import create_openai_fn_runnable
-from langchain_core.runnables import chain
-from langchain_openai.chat_models import ChatOpenAI
-from langserve import CustomUserType, add_routes
-from pydantic import BaseModel, Field, validator
+from langserve import add_routes
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from typing_extensions import Annotated, TypedDict
 
 from db.models import Extractor, get_session
-from extraction.utils import (
-    FewShotExample,
-    convert_json_schema_to_openai_schema,
-    make_prompt_template,
-)
+from extraction.utils import get_examples_from_extractor
 from server.api import examples, extractors
-from server.validators import validate_json_schema
+from server.extraction_runnable import (
+    ExtractRequest,
+    ExtractResponse,
+    extraction_runnable,
+)
+from server.retrieval import extract_from_content
 
 app = FastAPI(
     title="Extraction Powered by LangChain",
@@ -38,76 +35,19 @@ app.include_router(extractors.router)
 app.include_router(examples.router)
 
 
-class ExtractRequest(CustomUserType):
-    """Request body for the extract endpoint."""
-
+class ExtractFromFileRequest(BaseModel):
+    extractor_id: UUID = Field(..., description="The extractor ID to use.")
     text: str = Field(..., description="The text to extract from.")
-    json_schema: Dict[str, Any] = Field(
-        ...,
-        description="JSON schema that describes what content should be extracted "
-        "from the text.",
-        alias="schema",
+    mode: Literal["entire_document", "retrieval"] = Field(
+        "entire_document", description="The mode of extraction."
     )
-    instructions: Optional[str] = Field(
-        None, description="Supplemental system instructions."
-    )
-    examples: Optional[List[FewShotExample]] = Field(
-        None, description="Few shot examples."
-    )
-
-    @validator("json_schema")
-    def validate_schema(cls, v: Any) -> Dict[str, Any]:
-        """Validate the schema."""
-        validate_json_schema(v)
-        return v
-
-
-class ExtractResponse(BaseModel):
-    """Response body for the extract endpoint."""
-
-    extracted: Any
-
-
-model = ChatOpenAI(temperature=0)
-
-
-@chain
-def extraction_runnable(extraction_request: ExtractRequest) -> ExtractResponse:
-    """An end point to extract content from a given text object.
-
-    Used for powering an extraction playground.
-    """
-    schema = extraction_request.json_schema
-    name = schema.get("title", "")
-    try:
-        Draft202012Validator.check_schema(schema)
-    except exceptions.ValidationError as e:
-        raise HTTPException(status_code=422, detail=f"Invalid schema: {e.message}")
-
-    prompt = make_prompt_template(
-        extraction_request.instructions, extraction_request.examples, name
-    )
-    openai_function = convert_json_schema_to_openai_schema(schema)
-    runnable = create_openai_fn_runnable(
-        functions=[openai_function], llm=model, prompt=prompt
-    )
-    extracted_content = runnable.invoke({"text": extraction_request.text})
-
-    return ExtractResponse(
-        extracted=extracted_content,
-    )
-
-
-class ExtractFromFileRequest(TypedDict):
-    """Extract endpoint that uses an existing extractor."""
-
-    extractor_id: Annotated[UUID, "The extractor ID to use."]
-    text: Annotated[str, "The text to extract from."]
 
 
 @app.post("/extract", response_model=ExtractResponse, tags=["extraction"])
 async def extract_using_existing_extractor(
-    extract_request: ExtractFromFileRequest, *, session: Session = Depends(get_session)
+    extract_request: ExtractFromFileRequest,
+    *,
+    session: Session = Depends(get_session),
 ) -> ExtractResponse:
     """Endpoint that is used with an existing extractor.
 
@@ -116,17 +56,31 @@ async def extract_using_existing_extractor(
     """
     extractor = (
         session.query(Extractor)
-        .filter(Extractor.uuid == extract_request["extractor_id"])
+        .filter(Extractor.uuid == extract_request.extractor_id)
         .scalar()
     )
     if extractor is None:
         raise HTTPException(status_code=404, detail="Extractor not found.")
 
-    # Use the json schema and examples
-    json_schema = extractor.schema
-    examples = extractor.examples
-    assert examples == []
-    return ExtractResponse(extracted="placeholder")
+    mode = extract_request.mode
+    if mode == "entire_document":
+        json_schema = extractor.schema
+        examples = get_examples_from_extractor(extractor)
+        extraction_request = ExtractRequest(
+            text=extract_request.text,
+            schema=json_schema,
+            instructions=extractor.instruction,  # TODO: consistent naming
+            examples=examples,
+        )
+        return await extraction_runnable.ainvoke(extraction_request)
+    elif mode == "retrieval":
+        return await extract_from_content(
+            extract_request.text, extractor, text_splitter_kwargs=None
+        )
+    else:
+        raise ValueError(
+            f"Invalid mode {mode}. Expected one of 'entire_document', " "'retrieval'."
+        )
 
 
 add_routes(
--- backend/tests/unit_tests/test_extract_from_db.py
+++ backend/tests/unit_tests/test_extract_from_db.py
@@ -1,16 +1,29 @@
 """Code to test API endpoints."""
+from unittest.mock import patch
 from uuid import UUID
+
+from langchain_core.runnables import RunnableLambda
 
 from tests.db import get_async_client
 
 
+def mock_extraction_runnable(*args, **kwargs):
+    """Mock the extraction_runnable function."""
+    return {"extracted": "placeholder"}
+
+
+@patch("server.main.extraction_runnable", new=RunnableLambda(mock_extraction_runnable))
 async def test_extract_from_file() -> None:
     """Test extract from file API."""
     async with get_async_client() as client:
         # Test with invalid extractor
         extractor_id = UUID(int=1027)  # 1027 is a good number.
         response = await client.post(
-            "/extract", json={"extractor_id": str(extractor_id), "text": "Test Content"}
+            "/extract",
+            json={
+                "extractor_id": str(extractor_id),
+                "text": "Test Content",
+            },
         )
         assert response.status_code == 404
 
@@ -25,9 +38,14 @@ async def test_extract_from_file() -> None:
         # Get the extractor id
         extractor_id = response.json()
 
-        # Let's verify that there are no examples
+        # Extract
         response = await client.post(
-            "/extract", json={"extractor_id": extractor_id, "text": "Test Content"}
+            "/extract",
+            json={
+                "extractor_id": extractor_id,
+                "text": "Test Content",
+                "mode": "entire_document",
+            },
         )
         assert response.status_code == 200
         assert response.json() == {"extracted": "placeholder"}
