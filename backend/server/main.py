dd2abc6fe59bff60032a9931258ebe1f0f292714
Eugene Yurtsev <eyurtsev@gmail.com>
2024-02-23 10:27:15 -0500

    Add support for uploading files (#21)
    This adds support for uploading files.
    
    This is a breaking change in the endpoint as now the endpoint uses form data

--- backend/server/main.py
+++ backend/server/main.py
@@ -1,14 +1,14 @@
 """Entry point into the server."""
-from typing import Literal
+from typing import Literal, Optional
 from uuid import UUID
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, File, Form, HTTPException, UploadFile
 from langserve import add_routes
-from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-from typing_extensions import Annotated, TypedDict
+from typing_extensions import Annotated
 
 from db.models import Extractor, get_session
+from extraction.parsing import parse_binary_input
 from extraction.utils import get_examples_from_extractor
 from server.api import examples, extractors
 from server.extraction_runnable import (
@@ -35,18 +35,13 @@ app.include_router(extractors.router)
 app.include_router(examples.router)
 
 
-class ExtractFromFileRequest(BaseModel):
-    extractor_id: UUID = Field(..., description="The extractor ID to use.")
-    text: str = Field(..., description="The text to extract from.")
-    mode: Literal["entire_document", "retrieval"] = Field(
-        "entire_document", description="The mode of extraction."
-    )
-
-
 @app.post("/extract", response_model=ExtractResponse, tags=["extraction"])
 async def extract_using_existing_extractor(
-    extract_request: ExtractFromFileRequest,
     *,
+    extractor_id: Annotated[UUID, Form()],
+    text: Optional[str] = Form(None),
+    mode: Literal["entire_document", "retrieval"] = Form("entire_document"),
+    file: Optional[UploadFile] = File(None),
     session: Session = Depends(get_session),
 ) -> ExtractResponse:
     """Endpoint that is used with an existing extractor.
@@ -54,32 +49,36 @@ async def extract_using_existing_extractor(
     This endpoint will be expanded to support upload of binary files as well as
     text files.
     """
-    extractor = (
-        session.query(Extractor)
-        .filter(Extractor.uuid == extract_request.extractor_id)
-        .scalar()
-    )
+    if text is None and file is None:
+        raise HTTPException(status_code=422, detail="No text or file provided.")
+
+    extractor = session.query(Extractor).filter(Extractor.uuid == extractor_id).scalar()
     if extractor is None:
         raise HTTPException(status_code=404, detail="Extractor not found.")
 
-    mode = extract_request.mode
+    if text:
+        text_ = text
+    else:
+        documents = parse_binary_input(file.file)
+        # TODO: Add metadata like location from original file where
+        # the text was extracted from
+        text_ = "\n".join([document.page_content for document in documents])
+
     if mode == "entire_document":
         json_schema = extractor.schema
         examples = get_examples_from_extractor(extractor)
         extraction_request = ExtractRequest(
-            text=extract_request.text,
+            text=text_,
             schema=json_schema,
             instructions=extractor.instruction,  # TODO: consistent naming
             examples=examples,
         )
         return await extraction_runnable.ainvoke(extraction_request)
     elif mode == "retrieval":
-        return await extract_from_content(
-            extract_request.text, extractor, text_splitter_kwargs=None
-        )
+        return await extract_from_content(text_, extractor, text_splitter_kwargs=None)
     else:
         raise ValueError(
-            f"Invalid mode {mode}. Expected one of 'entire_document', " "'retrieval'."
+            f"Invalid mode {mode}. Expected one of 'entire_document', 'retrieval'."
         )
 
 
--- backend/tests/unit_tests/test_api_extract.py
+++ backend/tests/unit_tests/test_api_extract.py
@@ -1,4 +1,5 @@
 """Code to test API endpoints."""
+import tempfile
 from unittest.mock import patch
 from uuid import UUID
 
@@ -9,7 +10,10 @@ from tests.db import get_async_client
 
 def mock_extraction_runnable(*args, **kwargs):
     """Mock the extraction_runnable function."""
-    return {"extracted": "placeholder"}
+    extract_request = args[0]
+    return {
+        "extracted": extract_request.text[:10],
+    }
 
 
 @patch("server.main.extraction_runnable", new=RunnableLambda(mock_extraction_runnable))
@@ -20,12 +24,12 @@ async def test_extract_from_file() -> None:
         extractor_id = UUID(int=1027)  # 1027 is a good number.
         response = await client.post(
             "/extract",
-            json={
+            data={
                 "extractor_id": str(extractor_id),
                 "text": "Test Content",
             },
         )
-        assert response.status_code == 404
+        assert response.status_code == 404, response.text
 
         # First create an extractor
         create_request = {
@@ -34,18 +38,38 @@ async def test_extract_from_file() -> None:
             "instruction": "Test Instruction",
         }
         response = await client.post("/extractors", json=create_request)
-        assert response.status_code == 200
+        assert response.status_code == 200, response.text
         # Get the extractor id
         extractor_id = response.json()
 
-        # Extract
+        # Run an extraction.
+        # We'll use multi-form data here.
         response = await client.post(
             "/extract",
-            json={
+            data={
                 "extractor_id": extractor_id,
                 "text": "Test Content",
                 "mode": "entire_document",
             },
         )
         assert response.status_code == 200
-        assert response.json() == {"extracted": "placeholder"}
+        assert response.json() == {"extracted": "Test Conte"}
+
+        # We'll use multi-form data here.
+
+        # Create a named temporary file
+        with tempfile.NamedTemporaryFile(mode="w+t", delete=False) as f:
+            f.write("This is a named temporary file.")
+            f.seek(0)
+            f.flush()
+            response = await client.post(
+                "/extract",
+                data={
+                    "extractor_id": extractor_id,
+                    "mode": "entire_document",
+                },
+                files={"file": f},
+            )
+
+        assert response.status_code == 200, response.text
+        assert response.json() == {"extracted": "This is a "}
