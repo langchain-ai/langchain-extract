937bc1335a9d133fa35fe1ede2e626f7f52fee75
Eugene Yurtsev <eyurtsev@gmail.com>
2024-02-28 10:44:34 -0500

    fix main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,29 +2,27 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import App from "./App.tsx";
 import "./index.css";
 
-if (document.cookie.indexOf("user_id") === -1) {
-  document.cookie = `extract_user_id=${uuidv4()}`;
-}
+import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Root />,
-//     errorElement: <div>404</div>,
-//     children: [
-//       {
-//         path: "extractors/:uuid",
-//         // element: <Extractor />,
-//       },
-//     ],
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <div>404</div>,
+    children: [
+      {
+        path: "extractors/:uuid",
+        // element: <Extractor />,
+      },
+    ],
+  },
+]);
 
 import { render } from "react-dom";
 
