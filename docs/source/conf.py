7f5dc58b8942d22c49c9e59731ccac57b115f834
Eugene Yurtsev <eyurtsev@gmail.com>
2024-02-29 17:24:49 -0500

    x

---
+++ docs/source/conf.py
@@ -0,0 +1,113 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+import pathlib
+import sys
+from typing import List
+
+import toml
+
+ROOT_FOLDER = str(pathlib.Path(__file__).parent.parent.parent)
+
+# Add the project root to the path
+sys.path.insert(0, ROOT_FOLDER)
+
+with open("../../pyproject.toml") as f:
+    data = toml.load(f)
+
+project = "LangChain Extract"
+copyright = "2024, Langchain AI"
+author = "Langchain AI"
+
+version = data["tool"]["poetry"]["version"]
+release = version
+
+html_title = project + " " + version
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autodoc.typehints",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_nb",
+    "sphinx_copybutton",
+    "IPython.sphinxext.ipython_console_highlighting",
+]
+source_suffix = [".ipynb", ".html", ".md", ".rst"]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns: List[str] = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_book_theme"
+
+html_theme_options = {
+    "path_to_docs": "docs/source",
+    "repository_url": "https://github.com/langchain-ai/langchain-extract",
+    "home_page_in_toc": True,
+    "show_navbar_depth": 2,
+    "use_sidenotes": True,
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_source_button": True,
+    "use_fullscreen_button": True,
+    "repository_branch": "main",
+    "launch_buttons": {
+        "notebook_interface": "jupyterlab",
+        "colab_url": "https://colab.research.google.com",
+    },
+}
+
+html_context = {
+    "display_github": True,  # Integrate GitHub
+    "github_user": "langchain-ai",  # Username
+    "github_repo": "langchain-extract",  # Repo name
+    "github_version": "main",  # Version
+    "conf_py_path": "/docs/",  # Path in the checkout to the docs root
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/custom.css",
+]
+
+nb_execution_mode = "off"
+autosummary_generate = True
