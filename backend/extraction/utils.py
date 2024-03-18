278351bca08d9b5d74bc52ea9ba1e60cb1c8e513
Chester Curme <chester.curme@gmail.com>
2024-03-18 14:42:54 -0400

    pass json schema through

--- backend/extraction/utils.py
+++ backend/extraction/utils.py
@@ -50,3 +50,30 @@ def convert_json_schema_to_openai_schema(
         "description": "Extract information matching the given schema.",
         "parameters": _rm_titles(schema_) if rm_titles else schema_,
     }
+
+
+def update_json_schema(
+    schema: dict,
+    *,
+    multi: bool = True,
+) -> dict:
+    """Add missing fields to JSON schema and add support for multiple records."""
+    if multi:
+        # Wrap the schema in an object called "Root" with a property called: "data"
+        # which will be a json array of the original schema.
+        schema_ = {
+            "type": "object",
+            "properties": {
+                "data": {
+                    "type": "array",
+                    "items": dereference_refs(schema),
+                },
+            },
+            "required": ["data"],
+        }
+    else:
+        raise NotImplementedError("Only multi is supported for now.")
+
+    schema_["title"] = "extractor"
+    schema_["description"] = "Extract information matching the given schema."
+    return schema_
--- backend/server/extraction_runnable.py
+++ backend/server/extraction_runnable.py
@@ -15,7 +15,7 @@ from typing_extensions import TypedDict
 
 from db.models import Example, Extractor
 from extraction.utils import (
-    convert_json_schema_to_openai_schema,
+    update_json_schema,
 )
 from server.settings import CHUNK_SIZE, MODEL_NAME, get_model
 from server.validators import validate_json_schema
@@ -160,20 +160,19 @@ def get_examples_from_extractor(extractor: Extractor) -> List[Dict[str, Any]]:
 async def extraction_runnable(extraction_request: ExtractRequest) -> ExtractResponse:
     """An end point to extract content from a given text object."""
     # TODO: Add validation for model context window size
-    schema = extraction_request.json_schema
+    schema = update_json_schema(extraction_request.json_schema)
     try:
         Draft202012Validator.check_schema(schema)
     except exceptions.ValidationError as e:
         raise HTTPException(status_code=422, detail=f"Invalid schema: {e.message}")
 
-    openai_function = convert_json_schema_to_openai_schema(schema)
-    function_name = openai_function["name"]
     prompt = _make_prompt_template(
         extraction_request.instructions,
         extraction_request.examples,
-        function_name,
+        schema["title"],
     )
-    runnable = prompt | model.with_structured_output(schema=openai_function)
+    runnable = prompt | model.with_structured_output(schema=schema)
+
     return await runnable.ainvoke({"text": extraction_request.text})
 
 
