d9a69b505d290509caca25dbc9bd9130d525606c
bracesproul <braceasproul@gmail.com>
2024-03-18 15:10:06 -0700

    fe env example file

--- frontend/app/utils/api.tsx
+++ frontend/app/utils/api.tsx
@@ -24,7 +24,10 @@ const getBaseApiUrl = () => {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:8000";
   }
-  return `https://${process.env.BASE_API_URL}`;
+  if (!process.env.BASE_API_URL) {
+    throw new Error("BASE_API_URL must be set if not in development.");
+  }
+  return process.env.BASE_API_URL;
 };
 
 const getExtractor = async ({
