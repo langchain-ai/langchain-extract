7aa15f8d6d8226c6025387e9781de9e702e94f1f
bracesproul <braceasproul@gmail.com>
2024-03-20 13:23:33 -0700

    frontend[minor]: Add middleware func to always set userid cookie

---
+++ frontend/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { v4 as uuidv4 } from "uuid";
+ 
+// Automatically set a 'user_id' cookie if one doesn't exist
+// This function will run on every page request, so we can 
+// guarantee that a 'user_id' cookie will always be set.
+
+const USER_ID_COOKIE_KEY = 'user_id';
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const userId = request.cookies.get(USER_ID_COOKIE_KEY);
+
+  if (!userId) {
+    response.cookies.set(USER_ID_COOKIE_KEY, uuidv4());
+  }
+  return response;
+}
