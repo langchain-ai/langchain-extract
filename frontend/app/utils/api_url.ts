de40fc9384aa18c6507765db86cf26c4e58fcded
bracesproul <braceasproul@gmail.com>
2024-03-21 11:07:57 -0700

    cr

--- frontend/app/utils/api_url.ts
+++ frontend/app/utils/api_url.ts
@@ -1,9 +1,3 @@
 export const getBaseApiUrl = () => {
-  if (process.env.NODE_ENV === "development") {
-    return "http://localhost:8000";
-  }
-  if (!process.env.BASE_API_URL) {
-    throw new Error("BASE_API_URL must be set if not in development.");
-  }
-  return process.env.BASE_API_URL;
+  return "http://localhost:8000";
 };
