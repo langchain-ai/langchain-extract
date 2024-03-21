d0576e696d25d71266895a8d7d887984c3056336
Eugene Yurtsev <eyurtsev@gmail.com>
2024-03-21 14:36:45 -0400

    x

--- frontend/app/utils/api_url.ts
+++ frontend/app/utils/api_url.ts
@@ -2,5 +2,7 @@ export const getBaseApiUrl = () => {
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:8000";
   }
-  return window.location.origin;
+
+  // If not defined assume assume served from same domain
+  return "./"
 };
