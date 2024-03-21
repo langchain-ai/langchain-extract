fdd59281fc6eec2e204762c40269574133587b2d
Eugene Yurtsev <eyurtsev@gmail.com>
2024-03-21 11:12:30 -0400

    x

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -8,10 +8,10 @@ from sqlalchemy.engine import URL
 def get_postgres_url() -> URL:
     url = URL.create(
         drivername="postgresql",
-        username="langchain",
-        password="langchain",
+        username=os.environ.get("PG_USER", "langchain"),
+        password=os.environ.get("PG_PASSWORD", "langchain"),
         host=os.environ.get("PG_HOST", "localhost"),
-        database="langchain",
+        database=os.environ.get("PG_DATABASE", "langchain"),
         port=5432,
     )
     return url
