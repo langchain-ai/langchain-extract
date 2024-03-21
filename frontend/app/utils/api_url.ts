18cf32b39ee9a95f567d282a528f1efd627a71e7
bracesproul <braceasproul@gmail.com>
2024-03-21 11:11:33 -0700

    cr

--- frontend/app/utils/api_url.ts
+++ frontend/app/utils/api_url.ts
@@ -1,3 +1,6 @@
 export const getBaseApiUrl = () => {
-  return "http://localhost:8000";
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:8000";
+  }
+  return window.location.origin;
 };
