58cc2d3a1a27266f5efa570be720855ec33f4c96
Eugene Yurtsev <eyurtsev@gmail.com>
2024-03-25 13:35:07 -0400

    Add widgets for editing examples (#107)

--- frontend/app/components/Playground.tsx
+++ frontend/app/components/Playground.tsx
@@ -1,29 +1,33 @@
 "use client";
 import {
-  Button,
-  Heading,
-  Tab,
-  Box,
-  Divider,
   AbsoluteCenter,
+  Badge,
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  FormLabel,
+  HStack,
+  Heading,
+  Radio,
+  RadioGroup,
+  Spinner,
+  Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
   Text,
   Textarea,
-  FormControl,
-  FormLabel,
-  HStack,
-  Radio,
-  RadioGroup,
 } from "@chakra-ui/react";
+import Form from "@rjsf/chakra-ui";
+import validator from "@rjsf/validator-ajv8";
 import { useMutation } from "@tanstack/react-query";
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import { runExtraction, useConfiguration } from "../utils/api";
-import { Extractor } from "./Extractor";
+import { runExtraction, useConfiguration, useGetExtractor } from "../utils/api";
+import { ExampleEditor } from "./ExampleEditor";
 import { ResultsTable } from "./ResultsTable";
 
 interface PlaygroundProps {
@@ -43,6 +47,8 @@ export const Playground = (props: PlaygroundProps) => {
   const { data, isPending, mutate } = useMutation({
     mutationFn: runExtraction,
   });
+
+  const extractorQuery = useGetExtractor(extractorId, isShared);
 
   const requestServerConfig = useConfiguration();
   const [isDisabled, setIsDisabled] = React.useState(true);
@@ -97,96 +103,162 @@ export const Playground = (props: PlaygroundProps) => {
     setIsDisabled(false);
   };
 
+  if (extractorQuery.isLoading) {
+    return (
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    );
+  }
+
+  if (extractorQuery.isError) {
+    return <div>Unable to load extractor with ID: {extractorId}</div>;
+  }
+
+  if (extractorQuery.data === undefined) {
+    throw new Error("Data is undefined");
+  }
+
+  // Project the schema without the title, name, description
+  const cleanedSchema = {
+    ...extractorQuery.data.schema,
+    title: undefined,
+    description: undefined,
+  };
+
   return (
     <div className="w-full flex-col justify-between">
       <div className="m-auto">
-        {isShared && <Heading>Using a shared exractor</Heading>}
-        <div>
-          <Extractor extractorId={extractorId} isShared={isShared} />
-        </div>
-        <Heading>Extract</Heading>
-
-        <form
-          className="m-auto flex flex-col content-between gap-5 mt-10 mb-10"
-          onSubmit={handleSubmit}
-          onChange={handleChange}
-        >
-          {requestServerConfig.isFetched && (
-            <FormControl as="fieldset">
-              <FormLabel as="legend">Extraction Model</FormLabel>
-              <RadioGroup
-                name="modelId"
-                defaultValue={requestServerConfig.data?.models[0].name}
-              >
-                <HStack spacing="24px">
-                  {requestServerConfig.data?.models.map((model) => (
-                    <Radio value={model.name} key={model.name}>
-                      {model.description}
-                    </Radio>
-                  ))}
-                </HStack>
-              </RadioGroup>
-            </FormControl>
+        <Heading size="md" className="m-auto w-4/5" textAlign="center">
+          {extractorQuery.data?.name}
+          {isShared && (
+            <Badge
+              colorScheme="orange"
+              fontSize="xl"
+              fontWeight={"bold"}
+              marginLeft={2}
+            >
+              SHARED
+            </Badge>
           )}
-          {requestServerConfig.isFetched && (
-            <>
-              <input
-                type="file"
-                name="file"
-                accept={requestServerConfig.data?.accepted_mimetypes.join(", ")}
-                color="blue"
-                className="border-2 border-dashed border-gray-300 rounded-md p-4 w-full file:mr-4"
-              />
-              <Text fontSize="xs">
-                Max file size is: {requestServerConfig.data?.max_file_size_mb}MB
-              </Text>
-              <Text fontSize="xs">
-                Supported mimetypes:{" "}
-                {requestServerConfig.data?.accepted_mimetypes.join(", ")}
-              </Text>
-            </>
-          )}
-          <Box position="relative" padding="10">
-            <Divider />
-            <AbsoluteCenter bg="white" px="4">
-              OR
-            </AbsoluteCenter>
-          </Box>
-          <Textarea
-            placeholder="Enter text to extract information from..."
-            name="text"
-            className="textarea textarea-bordered h-3/4"
-            autoFocus
-            onKeyDown={handleKeyDown}
-          />
-          <Button type="submit" isDisabled={isDisabled}>
-            Run
-          </Button>
-        </form>
-      </div>
-      <div className="m-auto">
-        {data?.content_too_long && (
-          <Text color={"red"} margin={5}>
-            The content was too long to be processed. Extraction was run on a
-            truncated version of the content.
-          </Text>
-        )}
-        <Tabs variant={"enclosed"} colorScheme="blue" size="sm">
+        </Heading>
+        <Text className="m-auto w-4/5" textAlign="center">
+          {extractorQuery.data?.description}
+        </Text>
+        <Tabs variant={"enclosed"} colorScheme="blue" size="sm" marginTop={2}>
           <TabList>
-            <Tab>Table</Tab>
-            <Tab>JSON</Tab>
+            <Tab>Extract</Tab>
+            <Tab>JSON Schema</Tab>
+            <Tab isDisabled={isShared}>Examples</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
-              <ResultsTable data={data} isPending={isPending} />
+              <Form schema={cleanedSchema} validator={validator}>
+                {true} {/* Disables the submit button */}
+              </Form>
+              <form
+                className="m-auto flex flex-col content-between gap-5 mt-2 mb-10"
+                onSubmit={handleSubmit}
+                onChange={handleChange}
+              >
+                {requestServerConfig.isFetched && (
+                  <FormControl as="fieldset" marginTop={5}>
+                    <FormLabel as="legend">Model For Extraction</FormLabel>
+                    <RadioGroup
+                      name="modelId"
+                      defaultValue={requestServerConfig.data?.models[0].name}
+                    >
+                      <HStack spacing="24px">
+                        {requestServerConfig.data?.models.map((model) => (
+                          <Radio value={model.name} key={model.name}>
+                            {model.description}
+                          </Radio>
+                        ))}
+                      </HStack>
+                    </RadioGroup>
+                  </FormControl>
+                )}
+                {requestServerConfig.isFetched && (
+                  <>
+                    <input
+                      type="file"
+                      name="file"
+                      accept={requestServerConfig.data?.accepted_mimetypes.join(
+                        ", ",
+                      )}
+                      color="blue"
+                      className="border-2 border-dashed border-gray-300 rounded-md p-4 w-full file:mr-4"
+                    />
+                    <Text fontSize="xs">
+                      Max file size is:{" "}
+                      {requestServerConfig.data?.max_file_size_mb}MB
+                    </Text>
+                    <Text fontSize="xs">
+                      Supported mimetypes:{" "}
+                      {requestServerConfig.data?.accepted_mimetypes.join(", ")}
+                    </Text>
+                  </>
+                )}
+                <Box position="relative" padding="10">
+                  <Divider />
+                  <AbsoluteCenter bg="white" px="4">
+                    OR
+                  </AbsoluteCenter>
+                </Box>
+                <Textarea
+                  placeholder="Enter text to extract information from..."
+                  name="text"
+                  className="textarea textarea-bordered h-3/4"
+                  autoFocus
+                  onKeyDown={handleKeyDown}
+                />
+                <Button type="submit" isDisabled={isDisabled}>
+                  Run
+                </Button>
+              </form>
+
+              <div className="m-auto">
+                {data?.content_too_long && (
+                  <Text color={"red"} margin={5}>
+                    The content was too long to be processed. Extraction was run
+                    on a truncated version of the content.
+                  </Text>
+                )}
+                <Tabs variant={"enclosed"} colorScheme="blue" size="sm">
+                  <TabList>
+                    <Tab>Table</Tab>
+                    <Tab>JSON</Tab>
+                  </TabList>
+                  <TabPanels>
+                    <TabPanel>
+                      <ResultsTable data={data} isPending={isPending} />
+                    </TabPanel>
+                    <TabPanel>
+                      <Text className="mt-1 mb-5">
+                        Shows the output from the extractor in JSON format.
+                      </Text>
+                      <SyntaxHighlighter language="json" style={docco}>
+                        {JSON.stringify(data, null, 2)}
+                      </SyntaxHighlighter>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </div>
             </TabPanel>
             <TabPanel>
               <Text className="mt-1 mb-5">
-                Shows the output from the extractor in JSON format.
+                The raw JSON schema for the extractor.
               </Text>
               <SyntaxHighlighter language="json" style={docco}>
-                {JSON.stringify(data, null, 2)}
+                {JSON.stringify(extractorQuery.data.schema, null, 2)}
               </SyntaxHighlighter>
+            </TabPanel>
+            <TabPanel>
+              <ExampleEditor extractorId={extractorId} isShared={isShared} />
             </TabPanel>
           </TabPanels>
         </Tabs>
