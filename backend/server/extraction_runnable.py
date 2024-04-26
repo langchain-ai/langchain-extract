2ee39a3c7ff62a27aeb08287294a7c5f7f683632
Chester Curme <chester.curme@gmail.com>
2024-04-26 18:04:59 -0400

    remove comment

--- backend/server/extraction_runnable.py
+++ backend/server/extraction_runnable.py
@@ -172,7 +172,6 @@ async def extraction_runnable(extraction_request: ExtractRequest) -> ExtractResp
         schema["title"],
     )
     model = get_model(extraction_request.model_name)
-    # N.B. method must be consistent with examples in _make_prompt_template
     runnable = (
         prompt | model.with_structured_output(schema=schema)
     ).with_config({"run_name": "extraction"})
